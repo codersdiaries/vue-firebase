d6c1ee1c3a9a6995a36f5df5b49a001e96675f1d
Subham Mitra <subhammitra.dev97@gmail.com>
2021-01-02 15:09:21 +0530

    init

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/firestore'
 import 'firebase/storage'
 // replace the values with your firebase config
 var firebaseConfig = {
-    apiKey: "AIzaSyAg9hD-y0bre4Gsd1pRl98c9VHIj3wHa6Y",
-    authDomain: "test-f415e.firebaseapp.com",
-    projectId: "test-f415e",
-    storageBucket: "test-f415e.appspot.com",
-    messagingSenderId: "92231304841",
-    appId: "1:92231304841:web:f1706c17b43e9497516858"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 }
 firebase.initializeApp(firebaseConfig)
 
